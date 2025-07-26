4f920375c9f4f67b7dd9c8cae4413e04b94e0a28
kodhrt <04yo30@gmail.com>
2025-07-26 09:48:28 +0900

    create questions 3

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,8 +4,8 @@ from routers import questions
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"greet": "Hello, World"}
+# @app.get("/")
+# def read_root():
+#     return {"greet": "Hello, World"}
 
 app.include_router(questions.router)
