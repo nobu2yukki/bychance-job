f788291c397346752489d948be65990910d1fe3f
Mizuki Baba <info@lovelovetrb.com>
2025-07-26 14:02:09 +0000

    feat: 求人カード表示機能を追加し、スワイプ機能を実装

--- frontend/app/job-match/page.tsx
+++ frontend/app/job-match/page.tsx
@@ -1,8 +1,50 @@
+'use client';
+
+import JobCardWrapper from '@/app/job-match/JobCardWrapper';
+import { sampleJobs } from '@/mock_data/jobs';
+import type { Job } from '@/types/jobs';
+import type { SwipeResult } from '@/types/swipe_results';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [swipeResult, setSwipeResult] = useState<SwipeResult>({
+    good: [],
+    bad: [],
+  });
+
+  useEffect(() => {
+    // TODO: data fetch
+    const fetchJobs = async () => {
+      console.log(sampleJobs);
+      return sampleJobs;
+    };
+    fetchJobs().then(jobs => setJobs(jobs));
+  }, []);
+
+  const handleSwipe = (jobId: number, status: 'good' | 'bad') => {
+    console.log(jobId, status);
+    setSwipeResult(prev => {
+      const newSwipeResult = { ...prev };
+      newSwipeResult[status] = [...newSwipeResult[status], jobId];
+      return newSwipeResult;
+    });
+  };
+
+  // データの送信
+  const handleComplete = (jobId: number, status: 'good' | 'bad') => {
+    const finalResult = (() => {
+      const newSwipeResult = { ...swipeResult };
+      newSwipeResult[status] = [...newSwipeResult[status], jobId];
+      return newSwipeResult;
+    })();
+    console.log(finalResult);
+  };
+
+
   return (
     <main>
-      <h1 className="text-center">byChance-job</h1>
-      <p className="text-center">直感的に気に入ったら右へスワイプ</p>
+      <JobCardWrapper jobs={jobs} handleSwipe={handleSwipe} handleComplete={handleComplete} />
     </main>
   );
 }
---
+++ frontend/types/swipe_results.ts
@@ -0,0 +1,4 @@
+export type SwipeResult = {
+    good: number[];
+    bad: number[];
+};
