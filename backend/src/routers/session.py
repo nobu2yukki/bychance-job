4be283c036ef1ebe23c0bc9ff0f75c82a3656768
kodhrt <04yo30@gmail.com>
2025-07-26 15:08:27 +0000

    create start_session method

--- backend/src/routers/session.py
+++ backend/src/routers/session.py
@@ -6,13 +6,30 @@ router = APIRouter()
 
 # セッション一覧
 active_sessions = []
+sessions: dict[str, dict] = {}
+
+
+# @router.post("/session/init", tags=["session"])
+# def init_session(session_id: str):
+#     """指定されたセッションIDで空のセッションを初期化"""
+#     sessions[session_id] = {
+#         "question": {},
+#         "swipe": {"target": [], "result": {"good": [], "bad": []}},
+#         "result": {"recommendations": []},
+#     }
+#     return sessions
 
 
 @router.post("/session/start", tags=["session"])
 def start_session():
     session_id = str(uuid.uuid4())
-    active_sessions.append(session_id)
-    return {"session_id": session_id}
+    # active_sessions.append(session_id)
+    sessions[session_id] = {
+        "question": {},
+        "swipe": {"target": [], "result": {"good": [], "bad": []}},
+        "result": {"recommendations": []},
+    }
+    return sessions
 
 
 @router.get("/session/exists/{session_id}", tags=["session"])
