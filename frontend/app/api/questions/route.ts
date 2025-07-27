d8186af4e7cb295479992bf4022863b4f227f43e
maoto-wata <watanabe.maoto.21@shizuoka.ac.jp>
2025-07-27 06:53:18 +0000

    feat:if文の外側に紛れていた問題を解決

--- frontend/app/api/questions/route.ts
+++ frontend/app/api/questions/route.ts
@@ -13,16 +13,21 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const session_id = searchParams.get('session_id');
   const body = await request.json();
   console.log(body);
   const domain = process.env.BACKEND_URL;
-  const res = await fetch(`${domain}/questions/result`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/questions/result?session_id=${session_id}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
     },
-    body: JSON.stringify(body),
-  });
+  );
   if (!res.ok) {
     console.log(res);
     return NextResponse.json(
